9f162fa659f553d5271de2fc29616f4b9b05237b
ft <seniorlaguna@lagunabrothers.de>
2022-04-12 10:31:58 +0200

    trying to get dependencies done

---
+++ example/src/main/java/Runner.java
@@ -0,0 +1,14 @@
+import org.seniorlaguna.mexpa.BigDecimalCalculator;
+
+public class Runner {
+
+    public static void main(String[] args) {
+
+        BigDecimalCalculator calculator = new BigDecimalCalculator();
+        calculator.setDecimalPlaces(10);
+        calculator.setUseRadians(true);
+        System.out.println(calculator.evaluate("1/3"));
+
+    }
+
+}
